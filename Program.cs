9babe906c7bb700200a68df243bf2589790c59ed
Kontsantin <mikhajlov89@mail.ru>
2024-04-19 16:31:44 +0500

    Коммит для push в репозиторий

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Answer {
+    static void PrintEvenNumbers(int number)
+    {
+        int i = 1;
+        while (i <= number) {         
+               
+      if ( i % 2 == 0)  {
+              Console.WriteLine(i+" ");
+        
+      }
+         i=i+1;
+        }
+
+
+    }
+
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 5;
+        }
+
+        // Не удаляйте строки ниже
+        PrintEvenNumbers(number);
+    }
+}
